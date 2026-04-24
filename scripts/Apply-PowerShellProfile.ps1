a3b643da619e896c9e558b67e7a32b487eede07c
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2026-04-24 19:48:21 +0000

    Remove obsolete Windows Bootstrapper CI/CD workflow and initialization scripts; add PowerShell profile setup script and default theme configuration.

---
+++ scripts/Apply-PowerShellProfile.ps1
@@ -0,0 +1,7 @@
+# ./scripts/Apply-PowerShellProfile.ps1
+$rootDirectory = (Get-Location).Path
+
+$profileContent = Get-Content -Path "$rootDirectory\src\PowerShell\Profiles\Default.ps1" -Raw
+
+Set-Content -Path "$env:USERPROFILE\Documents\PowerShell\Microsoft.PowerShell_profile.ps1" -Value $profileContent -Force
+Set-Content -Path "$env:USERPROFILE\Documents\PowerShell\Microsoft.VSCode_profile.ps1" -Value $profileContent -Force
