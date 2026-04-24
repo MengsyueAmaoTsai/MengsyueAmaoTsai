c5bebe2bcb19c4c268976feda9f86dc0042eacc8
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2026-04-24 20:19:56 +0000

    Add SonarQube service management and configure git global settings

--- src/Bootstrappers/Windows/Start-Bootstrapper.ps1
+++ src/Bootstrappers/Windows/Start-Bootstrapper.ps1
@@ -1,4 +1,39 @@
-Write-Host "Starting bootstrapper..."
+$ErrorActionPreference = 'Stop'
 
-# Wait for 10 seconds to ensure the system is ready
-Start-Sleep -Seconds 10
+# =====================================================================================================================
+
+
+# =====================================================================================================================
+$sonarServiceName = "SonarQube"
+$sonarService = Get-Service -Name $sonarServiceName -ErrorAction Silently
+
+if ($null -ne $sonarService) {
+    if ($sonarService.Status -ne 'Running') {
+        Write-Host "Starting SonarQube service..." -ForegroundColor Green
+        Start-Service -Name $sonarServiceName
+        Write-Host "SonarQube service started." -ForegroundColor Green
+    }
+    else {
+        Write-Host "SonarQube service is already running." -ForegroundColor Green
+    }
+}
+else {
+    Write-Host "SonarQube service not found." -ForegroundColor Yellow
+}
+# =====================================================================================================================
+Write-Host "Configure git global settings ..." -ForegroundColor Green
+
+git config --global core.autocrlf true
+git config --global core.editor "code --wait"
+git config --global core.sshcommand "C:/Windows/System32/OpenSSH/ssh.exe"
+
+git config --global user.name "Mengsyue Amao Tsai"
+git config --global user.email "mengsyue.tsai@outlook.com"
+
+git config --global commit.gpgsign true
+
+git config --global color.ui auto
+
+git config --global gpg.program "C:\Program Files\GnuPG\bin\gpg.exe"
+
+Write-Host "Git global settings configured successfully." -ForegroundColor Green
